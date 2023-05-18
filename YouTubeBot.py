8253d1f8dfcb7134e72a2cdf3f2a948b3d9fa136
TeamXpertidea <133506933+TeamXpertidea@users.noreply.github.com>
2023-05-18 18:12:00 +0530

    Update YouTubeBot.py

--- YouTubeBot.py
+++ YouTubeBot.py
@@ -1,63 +1,58 @@
-import undetected_chromedriver as uc
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
+
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from tqdm import tqdm
-
 from csv import reader
-
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+import   undetected_chromedriver as uc
 
 def Connecting_To_Browser(id_str, pass_str):
-    browser = uc.Chrome()
-    try:
-        browser.get("https://gmail.com")
+    if id_str != "" and pass_str != "":
+        browser = uc.Chrome()
+        try:
+            browser.get('https://www.gmail.com/')
 
-        email_field = browser.find_element("id","identifierId")
-        email_field.clear()
-        time.sleep(2)
+            email_field = browser.find_element("id","identifierId")
+            email_field.clear()
+            
 
-        email_field.send_keys(id_str)
+            email_field.send_keys(id_str)
 
-        email_next_button = browser.find_element("id","identifierNext")
-        email_next_button.click()
+            email_next_button = browser.find_element("id","identifierNext")
+            email_next_button.click()
 
-        time.sleep(2)
+            time.sleep(2)
 
-        password_field = browser.find_element(By.NAME,"Passwd")   
-        # password_field.clear()
+            password_field = browser.find_element(By.NAME,"Passwd")
+            password_field.clear()
 
-        password_field.send_keys(pass_str)
-        time.sleep(2)
-        password_next_button = browser.find_element("id","passwordNext")
-        password_next_button.click()
+            password_field.send_keys(pass_str)
 
-        time.sleep(10)
-        
-        print('Login Successful...!!')
+            password_next_button = browser.find_element("id","passwordNext")
+            password_next_button.click()
+
+            time.sleep(3)
+
+            browser.get('https://www.youtube.com/watch?v=FVumnHy5Tzo')
+
+            browser.implicitly_wait(1000)
+            # subscribe_next_button = browser.find_element(By.CLASS_NAME,"style-scope ytd-subscribe-button-renderer")
+            # subscribe_next_button.click()
+            subscribe_next_button = browser.find_element("id","segmented-like-button") # press like button 
+            subscribe_next_button.click()
+
+            subscribe_next_button = browser.find_element("id","subscribe-button") # press subscribe button
+            subscribe_next_button.click()
 
 
-        # Go to the YouTube URL
-        # youtube_url="https://www.youtube.com/watch?v=s4ZUkwe0ZTI"
+            time.sleep(10)
+            browser.quit()
+        except Exception as e:
+            print(e)
+            browser.quit()
+    else:
+        print("Either ID or PASSWORD is null")
 
-        youtube_url="https://www.youtube.com/watch?v=BagZCVtaCPk"
-        browser.get(youtube_url)
-        browser.implicitly_wait(1000)
-
-        # WebDriverWait(browser, 15).until(EC.element_to_be_clickable(
-        # (By.XPATH, "//button[@aria-label='Play']"))).click()
-
-        subscribe_next_button = browser.find_element("id","segmented-like-button")
-        subscribe_next_button.click()
-        time.sleep(20)
-        print("channel liked")
-        # Close the browser
-        browser.quit()
-    except NoSuchElementException:
-        print("No Such Element Exception")
-        browser.quit()
-
+   
 
 with open('details.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
@@ -65,11 +60,20 @@ with open('details.csv', 'r') as read_obj:
     # Pass reader object to list() to get a list of lists
     list_of_rows = list(csv_reader)
 
+print("Total Ids and Passwords: ", len(list_of_rows)-1)
+total_Len = len(list_of_rows)
+
 ids_pass_list = list_of_rows
 
-
+progress_bar = tqdm(total=total_Len)
 for i in range(1,len(ids_pass_list)):
     id_str = ids_pass_list[i][0]
     id_pass = ids_pass_list[i][1]
-    
-    Connecting_To_Browser(id_str,id_pass)
+    print(i)
+    print("Login Id: ", id_str)
+    print("Login Password: ", id_pass)
+
+    Connecting_To_Browser(id_str, id_pass)
+    progress_bar.update(1)
+
+progress_bar.close()
